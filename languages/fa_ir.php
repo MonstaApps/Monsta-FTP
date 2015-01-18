8438f300826698aadcaf8242ce0970296d967da4
Monsta FTP <monstaftp@users.noreply.github.com>
2015-01-18 15:40:12 +1300

    Create fa_ir.php

---
+++ languages/fa_ir.php
@@ -0,0 +1,146 @@
+<?php
+
+// Language: Persian (IR)
+// Author: mostafa98 (http://mostafa98.ir)
+// Last Updated: 1393/09/19
+
+$file_lang_name = "Persian (IR)";
+$file_lang_default = 0; // 1 for default, 0 for not
+
+$lang_title_ended = "جلسه به پایان رسید";
+$lang_title_errors = "خطاهایی رخ داده";
+
+$lang_session_expired = "جلسه شما منقضی شده است";
+$lang_ip_conflict = "آدرس آی پی شما با آی پی جلسه متضاد است";
+
+$lang_btn_login = "ورود";
+$lang_ftp_host = "اف تی پی هاست";
+$lang_port = "پورت";
+$lang_passive_mode = "(حالت منفعل (پیش فرض";
+$lang_username = "نام کاربری";
+$lang_password = "رمز عبور";
+$lang_ftp_ssl = "(در صورت موجود بودن)اتصال با استفاده از SSL";
+$lang_adv_interface = "رابط کاربری پیشرفته";
+$lang_save_login = "ذخیره جزئیات ورود";
+$lang_language = "زبان";
+$lang_skin = "پوسته";
+$lang_ip_check = "قفل جلسه برای آی پی";
+
+$lang_skins_empty = "[پوسته های] پوشه خالی!";
+$lang_skins_locked = "[پوسته های] پوشه قفل شده است!";
+$lang_skins_missing = "[پوسته های] پوشه از دست رفته!";
+
+$lang_missing_fields = "خطا! لطفا تمام زمینه های مشخص شده را تکمیل نمایید";
+$lang_max_logins = "لطفا در [n] دقیقه دوباره سعی کنید.شما بیش از حد برای ورود تلاش کردید وقفل شدید";
+$lang_cant_connect = "خطا! اتصال اف تی پی هاست نمیتواند وصل شود";
+$lang_cant_authenticate = "خطا! ورود شما تایید نمی شود";
+$lang_ip_conflict = "تعارض آی پی تشخیص داده شده است";
+
+$lang_table_name = "نام";
+$lang_table_size = "حجم";
+$lang_table_date = "تاریخ";
+$lang_table_time = "زمان";
+$lang_table_user = "کاربر";
+$lang_table_group = "گروه";
+$lang_table_perms = "مجوز ها";
+
+$lang_size_b = "B";
+$lang_size_kb = "KB";
+$lang_size_mb = "MB";
+$lang_size_gb = "GB";
+$lang_size_tb = "TB";
+
+$lang_btn_refresh = "نو سازی";
+$lang_btn_cut = "برداشتن";
+$lang_btn_copy = "کپی";
+$lang_btn_paste = "چسباندن";
+$lang_btn_rename = "تغییر نام";
+$lang_btn_delete = "حذف";
+$lang_btn_chmod = "Chmod";
+$lang_btn_logout = "خروج";
+$lang_btn_save = "ذخیره سازی";
+$lang_btn_close = "بستن";
+$lang_btn_cancel = "لغو";
+$lang_btn_ok = "OK";
+$lang_btn_new_folder = "پوشه جدید";
+$lang_btn_new_file = "فایل جدید";
+$lang_btn_upload_file = "آپلود فایل";
+$lang_btn_upload_files = "آپلود فایل ها";
+$lang_btn_upload_repeat = "تکرار بارگذاری";
+$lang_btn_upload_folder = "آپلود پوشه";
+
+$lang_info_host = "هاست";
+$lang_info_user = "کاربر";
+$lang_info_upload_limit = "محدودیت آپلود";
+$lang_info_drag_drop = "کشیدن و رها کردن";
+
+$lang_xfer_file = "فایل";
+$lang_xfer_size = "حجم";
+$lang_xfer_progress = "میزان پیشرفت";
+$lang_xfer_elapsed = "مدت سپری شده";
+$lang_xfer_uploaded = "آپلود شد";
+$lang_xfer_rate = "نرخ انتقال";
+$lang_xfer_remain = "زمان باقی مانده";
+
+$lang_move_conflict = "پوشه مقصد زیرپوشه ای از پوشه منبع است";
+$lang_cant_rename = "ناتوان در تغییر نام [فایل]";
+$lang_cant_delete = "ناتوان در حذف [فایل]";
+
+$lang_folder_exists = "پوشه [پوشه] در حال حاضر وجود دارد";
+$lang_folder_doesnt_exist = "پوشه [پوشه] وجود ندارد";
+$lang_folder_cant_move = "عدم امکان انتقال [پوشه]";
+$lang_folder_cant_delete = "ناتوان درحذف [پوشه]";
+$lang_folder_cant_access = "ناتوان در دسترسی به [پوشه]";
+$lang_folder_cant_make = "ناتوان در ایجاد [پوشه]";
+
+$lang_file_exists = "فایل [فایل] در حال حاضر وجود دارد";
+$lang_file_doesnt_exist = "فایل [فایل] وجود ندارد";
+$lang_file_cant_move = "عدم امکان انتقال [فایل]";
+$lang_file_cant_make = "نمی تواند ایجاد کنید [فایل]";
+
+$lang_server_error_down = " خطای دانلود سرور [فایل]";
+$lang_server_error_up = "خطای آپلود کردن سرور [فایل]";
+$lang_browser_error_up = "خطای آپلود کردن مرورگر [فایل]";
+$lang_file_size_error = "حجم فایل بیش از حد مجاز است";
+$lang_file_size_copy_error = "[فایل] حجم بیش از حد مجاز است";
+
+$lang_folder_cant_chmod = "ناتوان در سطح دسترسی [حالت دهنده های دائمی] [پوشه]";
+$lang_file_cant_chmod = "ناتوان در سطح دسترسی [حالت دهنده های دائمی] [فایل]";
+$lang_chmod_max_777 = "سطح دسترسی نمی تواند بالاتر از 777 باشد";
+$lang_chmod_owner = "مجوزهای مالک";
+$lang_chmod_group = "مجوز های گروه";
+$lang_chmod_public = "مجوز های عمومی";
+$lang_chmod_manual = "دستی";
+$lang_chmod_read = "خواندن";
+$lang_chmod_write = "نوشتن";
+$lang_chmod_exe = "Execute";
+$lang_chmod_no_support = "سرور پی اچ پی شما از ftp_chmod پشتیبانی نمی کند";
+
+$lang_title_rename = "تغییر نام فایل ها";
+$lang_title_chmod = "صفات تغییر پرونده";
+$lang_title_edit_file = "ویرایش کردن";
+$lang_title_new_file = "فایل جدید";
+$lang_title_new_folder = "پوشه جدید";
+$lang_new_folder_name = "نام پوشه جدید را وارد کنید ...";
+$lang_new_file_name = "نام فایل جدید را وارد کنید...";
+$lang_template = "قالب";
+$lang_no_template = "بدون قالب";
+
+$lang_no_xmlhttp = "مرورگر شما از XMLHTTP پشتیبانی نمی کند!";
+$lang_support_drop = "پشتیبانی می شود";
+$lang_no_support_drop = "پشتیبانی نمی شود";
+$lang_transfer_pending = "در انتظار";
+$lang_transferring_to_ftp = "انتقال به سرور اف تی پی ...";
+$lang_no_file_selected = " کلیک کرده و فایل مورد نظرتان را انتخاب کنید Browse لطفا بر روی کلید ";
+$lang_none_selected = "لطفا حداقل یک فایل یا پوشه را انتخاب کنید!";
+$lang_context_open = "باز کردن";
+$lang_context_download = "دانلود";
+$lang_context_edit = "ویرایش";
+$lang_context_cut = "برداشتن";
+$lang_context_copy = "کپی";
+$lang_context_paste = "چسباندن";
+$lang_context_rename = "تغییر نام";
+$lang_context_delete = "حذف";
+$lang_context_chmod = "سطح دسترسی";
+
+?>
