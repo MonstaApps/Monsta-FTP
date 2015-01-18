09e008c7556954fb5479ef8ccf824b0e599de9a5
Monsta FTP <monstaftp@users.noreply.github.com>
2015-01-18 15:42:35 +1300

    Create sk_sk.php

---
+++ languages/sk_sk.php
@@ -0,0 +1,147 @@
+<?php
+
+// Language: Slovak (SK)
+// Author: Branislav Pírek, www.piri.sk
+// Last Updated: 01/09/2014
+
+$file_lang_name = "Slovak (SK)";
+$file_lang_default = 0; // 1 for default, 0 for not
+
+$lang_title_ended = "Spojenie ukončené";
+$lang_title_errors = "Došlo k chybe";
+
+$lang_session_expired = "Vaše spojenie vypršalo";
+$lang_ip_conflict = "Vaša IP adresa je v rozpore s inou pripojenou IP adresou";
+
+$lang_btn_login = "Prihlásiť";
+$lang_ftp_host = "FTP Hostiteľ";
+$lang_port = "Port";
+$lang_passive_mode = "Pasívny režim (predvolené)";
+$lang_username = "Meno užívateľa";
+$lang_password = "Heslo";
+$lang_ftp_ssl = "Pripojiť sa cez SSL (ak je to možné)";
+$lang_adv_interface = "Rozšírené rozhranie";
+$lang_save_login = "Uložiť prihlasovacie údaje";
+$lang_language = "Jazyk";
+$lang_skin = "Grafická schéma";
+$lang_ip_check = "Uzamknúť toto spojenie s vašou IP";
+
+$lang_skins_empty = "[skins] priečinok je prázdny!";
+$lang_skins_locked = "[skins] priečinok je blokovaný!";
+$lang_skins_missing = "[skins] priečinok neexistuje!";
+
+$lang_missing_fields = "Chyba! Prosím, vyplňte všetky označené polia";
+$lang_max_logins = "Prekročili ste povolený počet pokusov o prihlásenie a účet bol zablokovaný. Skúste to znovu o [n] minút";
+$lang_cant_connect = "Chyba! Nie je možné sa pripojiť k FTP serveru";
+$lang_cant_authenticate = "Chyba! Nie je možné overiť prihlasovacie údaje";
+$lang_ip_conflict = "Bol zistený konflikt IP adries"; 
+
+$lang_table_name = "Názov";
+$lang_table_size = "Veľkosť";
+$lang_table_date = "Dátum";
+$lang_table_time = "Čas";
+$lang_table_user = "Užívateľ";
+$lang_table_group = "Skupina";
+$lang_table_perms = "Oprávnenia";
+
+$lang_size_b = "B";
+$lang_size_kb = "kB";
+$lang_size_mb = "MB";
+$lang_size_gb = "GB";
+$lang_size_tb = "TB";
+
+$lang_btn_sendlink = "Odoslať odkaz";
+$lang_btn_refresh = "Obnoviť";
+$lang_btn_cut = "Vystrihnúť";
+$lang_btn_copy = "Kopírovať";
+$lang_btn_paste = "Prilepiť";
+$lang_btn_rename = "Premenovať";
+$lang_btn_delete = "Odstrániť";
+$lang_btn_chmod = "Oprávnenia";
+$lang_btn_logout = "Odhlísiť";
+$lang_btn_save = "Uložiť";
+$lang_btn_close = "Zavrieť";
+$lang_btn_cancel = "Zrušiť";
+$lang_btn_ok = "OK";
+$lang_btn_new_folder = "Nový priečinok";
+$lang_btn_new_file = "Nový súbor";
+$lang_btn_upload_file = "Nahrať súbor";
+$lang_btn_upload_files = "Nahrať súbory";
+$lang_btn_upload_repeat = "Opakovať nahrávanie";
+$lang_btn_upload_folder = "Nahrať priečinok";
+
+$lang_info_host = "Hostiteľ";
+$lang_info_user = "Užívateľ";
+$lang_info_upload_limit = "Maximálna veľkosť";
+$lang_info_drag_drop = "Sem môžete priamo presunúť súbory potiahnutím myškou";
+
+$lang_xfer_file = "Súbor";
+$lang_xfer_size = "Veľkosť";
+$lang_xfer_progress = "Stav";
+$lang_xfer_elapsed = "Uplynulý čas";
+$lang_xfer_uploaded = "Nahrané";
+$lang_xfer_rate = "Rýchlosť prenosu";
+$lang_xfer_remain = "Zostávajúci čas";
+
+$lang_move_conflict = "Cieľový priečinok je podpriečinkom zdrojového priečinka";
+$lang_cant_rename = "Nie je možné premenovať súbor [file]";
+$lang_cant_delete = "Nie je možné odstrániť súbor [file]";
+
+$lang_folder_exists = "Priečinok [folder] už existuje";
+$lang_folder_doesnt_exist = "Priečinok [folder] neexistuje";
+$lang_folder_cant_move = "Priečinok [folder] nie je možné presunúť";
+$lang_folder_cant_delete = "Priečinok [folder] nie je možné odstrániť";
+$lang_folder_cant_access = "Priečinok [folder] nie je možné otvoriť";
+$lang_folder_cant_make = "Priečinok [folder] nie je možné vytvoriť";
+
+$lang_file_exists = "Súbor [file] už existuje";
+$lang_file_doesnt_exist = "Súbor [file] neexistuje";
+$lang_file_cant_move = "Súbor [file] nie je možné presunúť";
+$lang_file_cant_make = "Súbor [file] nie je možné vytvoriť";
+
+$lang_server_error_down = "Chyba servera pri sťahovaní súboru [file]";
+$lang_server_error_up = "Chyba servera pri nahrávaní súboru [file]";
+$lang_browser_error_up = "Chyba prehliadača pri nahrávaní súboru [file]";
+$lang_file_size_error = "Súbor je väčší ako stanovený limit";
+$lang_file_size_copy_error = "Súbor [file] je väčší ako stanovený limit";
+
+$lang_folder_cant_chmod = "Nie je možné zmeniť oprávnenia [perms] priečinka [folder]";
+$lang_file_cant_chmod = "Nie je možné zmeniť oprávnenia [perms] súboru [file]";
+$lang_chmod_max_777 = "Parameter nemôže byť väčší ako 777";
+$lang_chmod_owner = "Oprávnenia vlastníka";
+$lang_chmod_group = "Skupinové oprávnenia";
+$lang_chmod_public = "Globálne oprávnenia";
+$lang_chmod_manual = "Manuálne";
+$lang_chmod_read = "Čitanie";
+$lang_chmod_write = "Zápis";
+$lang_chmod_exe = "Vykonávať";
+$lang_chmod_no_support = "Váš PHP server nepodporuje funkciu ftp_chmod";
+
+$lang_title_rename = "Premenovať súbory";
+$lang_title_chmod = "Zmeniť atribúty súboru";
+$lang_title_edit_file = "Editácia";
+$lang_title_new_file = "Nový súbor";
+$lang_title_new_folder = "Nový priečinok";
+$lang_new_folder_name = "Zadajte nový názov priečinka...";
+$lang_new_file_name = "Zadajte nový názov súboru...";
+$lang_template = "Šablóna";
+$lang_no_template = "Žiadna šablóna";
+
+$lang_no_xmlhttp = "Váš prehliadač nepodporuje XMLHTTP!";
+$lang_support_drop = "Podporované";
+$lang_no_support_drop = "Nepodporované";
+$lang_transfer_pending = "V poradí";
+$lang_transferring_to_ftp = "Odosielanie na FTP server...";
+$lang_no_file_selected = "Kliknite na PREHĽADÁVAŤ a vyberte súbor pre nahranie!";
+$lang_none_selected = "Musíte vybrať aspoň jeden súbor alebo priečinok!";
+$lang_context_open = "Otvoriť";
+$lang_context_download = "Stiahnuť";
+$lang_context_edit = "Upraviť";
+$lang_context_cut = "Vystrihnúť";
+$lang_context_copy = "Kopírovať";
+$lang_context_paste = "Prilepiť";
+$lang_context_rename = "Premenovať";
+$lang_context_delete = "Odstrániť";
+$lang_context_chmod = "Oprávnenia";
+
+?>
