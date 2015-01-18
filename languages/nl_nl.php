f8cf68346b573c3a17a11c8625408313a8753a03
Monsta FTP <monstaftp@users.noreply.github.com>
2015-01-18 15:41:34 +1300

    Create nl_nl.php

---
+++ languages/nl_nl.php
@@ -0,0 +1,146 @@
+<?php
+
+// Language: Dutch~Nederlands (NL)
+// Author: Ben (De Vries ICT)
+// Last Updated: 15/12/2013
+
+$file_lang_name = "Nederlands (NL)";
+$file_lang_default = 0; // 1 for default, 0 for not
+
+$lang_title_ended = "Sessie beeindigd";
+$lang_title_errors = "Er zijn fouten opgetreden";
+
+$lang_session_expired = "Uw sessie is vervallen";
+$lang_ip_conflict = "Uw IP-adres geeft een conflict met de sessie IP";
+
+$lang_btn_login = "Login";
+$lang_ftp_host = "FTP Host";
+$lang_port = "Poort";
+$lang_passive_mode = "Passieve modus (standaard)";
+$lang_username = "Gebruikersnaam";
+$lang_password = "Wachtwoord";
+$lang_ftp_ssl = "Verbind met SSL (indien beschikbaar)";
+$lang_adv_interface = "Geavanveerde interface";
+$lang_save_login = "Inlogdetails opslaan";
+$lang_language = "Taal";
+$lang_skin = "Skin";
+$lang_ip_check = "Zet sessie vast op IP";
+
+$lang_skins_empty = "[skins] map leeg!";
+$lang_skins_locked = "[skins] map geblokkeerd!";
+$lang_skins_missing = "[skins] map mist!";
+
+$lang_missing_fields = "Fout! Vul alle gemarkeerde verplichte velden in";
+$lang_max_logins = "U heeft de maximaal aantal toegestane inlogpogingen bereikt en het inloggen is nu geblokkeerd. Probeer opnieuw na [n] minuten";
+$lang_cant_connect = "Fout! Kan de FTP-host niet bereiken";
+$lang_cant_authenticate = "Fout! Kan uw inlog niet certificeren";
+$lang_ip_conflict = "ER is een IP-conflict opgetreden";
+
+$lang_table_name = "Naam";
+$lang_table_size = "Grootte";
+$lang_table_date = "Datum";
+$lang_table_time = "Tijd";
+$lang_table_user = "Gebruiker";
+$lang_table_group = "Groep";
+$lang_table_perms = "Rechten";
+
+$lang_size_b = "B";
+$lang_size_kb = "KB";
+$lang_size_mb = "MB";
+$lang_size_gb = "GB";
+$lang_size_tb = "TB";
+
+$lang_btn_refresh = "Vernieuw";
+$lang_btn_cut = "Knip";
+$lang_btn_copy = "Kopieer";
+$lang_btn_paste = "Plak";
+$lang_btn_rename = "Hernoem";
+$lang_btn_delete = "Verwijder";
+$lang_btn_chmod = "Chmod";
+$lang_btn_logout = "Log uit";
+$lang_btn_save = "Opslaan";
+$lang_btn_close = "Sluit";
+$lang_btn_cancel = "Annuleer";
+$lang_btn_ok = "OK";
+$lang_btn_new_folder = "Nieuwe map";
+$lang_btn_new_file = "Nieuw bestand";
+$lang_btn_upload_file = "Upload bestand";
+$lang_btn_upload_files = "Upload bestanden";
+$lang_btn_upload_repeat = "Herhaal Upload";
+$lang_btn_upload_folder = "Upload map";
+
+$lang_info_host = "Host";
+$lang_info_user = "Gebruiker";
+$lang_info_upload_limit = "Upload Limiet";
+$lang_info_drag_drop = "Drag & Drop";
+
+$lang_xfer_file = "Bestand";
+$lang_xfer_size = "Grootte";
+$lang_xfer_progress = "Voortgang";
+$lang_xfer_elapsed = "Tijd verstreken";
+$lang_xfer_uploaded = "Geupload";
+$lang_xfer_rate = "Dataoverdracht";
+$lang_xfer_remain = "Resterende tijd";
+
+$lang_move_conflict = "Doelmap is een submap van de bronmap";
+$lang_cant_rename = "Onmogelijk te hernoemen [file]";
+$lang_cant_delete = "Onmogelijk te verwijderen [file]";
+
+$lang_folder_exists = "Map [folder] bestaal reeds";
+$lang_folder_doesnt_exist = "Map [folder] bestaat niet";
+$lang_folder_cant_move = "Onmogelijk te verplaatsen [folder]";
+$lang_folder_cant_delete = "Onmogelijk te verwijderen [folder]";
+$lang_folder_cant_access = "Onmogelijk te benaderen [folder]";
+$lang_folder_cant_make = "Onmogelijk te maken [folder]";
+
+$lang_file_exists = "Bestand [file] bestaat al";
+$lang_file_doesnt_exist = "Bestand [file] bestaat niet";
+$lang_file_cant_move = "Onmogelijk te verplaatsen [file]";
+$lang_file_cant_make = "Onmogelijk te maken [file]";
+
+$lang_server_error_down = "Serverfout downloaden [file]";
+$lang_server_error_up = "Serverfout uploaden [file]";
+$lang_browser_error_up = "Browserfout uploaden [file]";
+$lang_file_size_error = "Bestandsgrootte is meer dan toegestaan";
+$lang_file_size_copy_error = "[file] grootte is meer dan toegestaan";
+
+$lang_folder_cant_chmod = "Onmogelijke chmod [perms] [folder]";
+$lang_file_cant_chmod = "Onmogelijke chmod [perms] [file]";
+$lang_chmod_max_777 = "Chmod mag niet groter zijn dan 777";
+$lang_chmod_owner = "Eigenaarsrechten";
+$lang_chmod_group = "Groepsrechten";
+$lang_chmod_public = "Publieke rechten";
+$lang_chmod_manual = "Handleiding";
+$lang_chmod_read = "Lees";
+$lang_chmod_write = "Schrijf";
+$lang_chmod_exe = "Uitvoeren";
+$lang_chmod_no_support = "Uw PHP-server ondersteund geen ftp_chmod";
+
+$lang_title_rename = "Hernoem bestanden";
+$lang_title_chmod = "Verander bestandseigenschappen";
+$lang_title_edit_file = "Muteren";
+$lang_title_new_file = "Nieuw bestand";
+$lang_title_new_folder = "Nieuwe map";
+$lang_new_folder_name = "Vul de nieuwe mapnaam in...";
+$lang_new_file_name = "Vul de nieuwe bestandsnaam in...";
+$lang_template = "Sjabloon";
+$lang_no_template = "Geen sjabloon";
+
+$lang_no_xmlhttp = "Uw browser ondersteund geen XMLHTTP!";
+$lang_support_drop = "Ondersteund";
+$lang_no_support_drop = "Niet ondersteund";
+$lang_transfer_pending = "Wachtende";
+$lang_transferring_to_ftp = "Verplaatsing naar FTP-server...";
+$lang_no_file_selected = "Klik BROWSE/ZOEK en kies een bestand om te uploaden!";
+$lang_none_selected = "Kies op zijn minste een bestand of map!";
+$lang_context_open = "Open";
+$lang_context_download = "Download";
+$lang_context_edit = "Wijzig";
+$lang_context_cut = "Knip";
+$lang_context_copy = "Kopieer";
+$lang_context_paste = "Plak";
+$lang_context_rename = "Hernoem";
+$lang_context_delete = "Verwijder";
+$lang_context_chmod = "Chmod";
+
+?>
