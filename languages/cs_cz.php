1abdb69e7b7f0121ce0b0df190132103239b669b
Monsta FTP <monstaftp@users.noreply.github.com>
2015-01-18 15:39:06 +1300

    Create cs_cz.php

---
+++ languages/cs_cz.php
@@ -0,0 +1,147 @@
+<?php
+
+// Language: Czech(CZ)
+// Author: Michal Viklický
+// Last Updated: 10/01/2014
+
+$file_lang_name = "Cestina (CZ)";
+$file_lang_default = 0; // 1 for default, 0 for not
+
+$lang_title_ended = "Spojení ukonceno";
+$lang_title_errors = "Nastala chyba";
+
+$lang_session_expired = "Spojení vypršelo";
+$lang_ip_conflict = "Tvá adresa IP je v konfliktu s jiným spojením adresy IP";
+
+$lang_btn_login = "Prihlásit";
+$lang_ftp_host = "FTP Host";
+$lang_port = "Port";
+$lang_passive_mode = "Pasivní mód (výchozí)";
+$lang_username = "Uživatelské jméno";
+$lang_password = "Heslo";
+$lang_ftp_ssl = "Spojení pomocí SSL (je-li k dispozici)";
+$lang_adv_interface = "Rozšížené prostredí";
+$lang_save_login = "Uložit prihlašovací údaje";
+$lang_language = "Jazyk";
+$lang_skin = "Barevné schéma";
+$lang_ip_check = "Zamknout spojení k adrese IP";
+
+$lang_skins_empty = "[skins] složka je prázdná!";
+$lang_skins_locked = "[skins] složka je uzamcena!";
+$lang_skins_missing = "[skins] složka neexistuje!";
+
+$lang_missing_fields = "Chyba! Prosím vypln všechna oznacená pole";
+$lang_max_logins = "Bylo dosaženo maximálního poctu pokusu o prihlášení a úcet byl uzamcen. Zkuste to znovu za [n] minut";
+$lang_cant_connect = "Chyba! Nemohu se pripojit k úložišti";
+$lang_cant_authenticate = "Chyba! Nemohu overit prihlašovací údaje";
+$lang_ip_conflict = "Byl detekován konflikt IP adres";
+
+$lang_table_name = "Jméno";
+$lang_table_size = "Velikost";
+$lang_table_date = "Datum";
+$lang_table_time = "Cas";
+$lang_table_user = "Uživatel";
+$lang_table_group = "Skupina";
+$lang_table_perms = "Prístupová práva";
+
+$lang_size_b = "B";
+$lang_size_kb = "KB";
+$lang_size_mb = "MB";
+$lang_size_gb = "GB";
+$lang_size_tb = "TB";
+
+$lang_btn_sendlink = "Odeslat link";
+$lang_btn_refresh = "Obnovit";
+$lang_btn_cut = "Vyjmout";
+$lang_btn_copy = "Kopírovat";
+$lang_btn_paste = "Vložit";
+$lang_btn_rename = "Prejmenovat";
+$lang_btn_delete = "Smazat";
+$lang_btn_chmod = "Zabezpecení";
+$lang_btn_logout = "Odhlásit";
+$lang_btn_save = "Uložit";
+$lang_btn_close = "Zavrít";
+$lang_btn_cancel = "Zrušit";
+$lang_btn_ok = "OK";
+$lang_btn_new_folder = "Nová složka";
+$lang_btn_new_file = "Nový soubor";
+$lang_btn_upload_file = "Nahrát soubor";
+$lang_btn_upload_files = "Nahrát soubor";
+$lang_btn_upload_repeat = "Opakovat nahrání";
+$lang_btn_upload_folder = "Nahrát složku";
+
+$lang_info_host = "Úložište";
+$lang_info_user = "Uživatel";
+$lang_info_upload_limit = "Max.velikost";
+$lang_info_drag_drop = "Chytni a táhni";
+
+$lang_xfer_file = "Soubor";
+$lang_xfer_size = "Velikost";
+$lang_xfer_progress = "Prubeh";
+$lang_xfer_elapsed = "Ubehnutý cas";
+$lang_xfer_uploaded = "Nahráno";
+$lang_xfer_rate = "Prenosová rychlost";
+$lang_xfer_remain = "Zbývající cas";
+
+$lang_move_conflict = "Cílová složka je podsložkou zdrojové složky";
+$lang_cant_rename = "Nemohu prejmenovat soubor [file]";
+$lang_cant_delete = "Nemohu smazat soubor [file]";
+
+$lang_folder_exists = "Složka [folder] již existuje";
+$lang_folder_doesnt_exist = "Složka [folder] neexistuje";
+$lang_folder_cant_move = "Nemohu presunout [folder]";
+$lang_folder_cant_delete = "Nemohu smazat [folder]";
+$lang_folder_cant_access = "Nemohu pristupovat do [folder]";
+$lang_folder_cant_make = "Nemohu vytvorit složku [folder]";
+
+$lang_file_exists = "Soubor [file] již existuje";
+$lang_file_doesnt_exist = "Soubor [file] neexistuje";
+$lang_file_cant_move = "Nemohu presunout [file]";
+$lang_file_cant_make = "Nemohu vytvorit [file]";
+
+$lang_server_error_down = "Chyba úložište pri stahování souboru [file]";
+$lang_server_error_up = "Chyba úložište pri nahrávání souboru [file]";
+$lang_browser_error_up = "Chyba klienta pri nahrávání souboru [file]";
+$lang_file_size_error = "Soubor je vetší, než dovoluje maximum";
+$lang_file_size_copy_error = "Souboru [file] je vetší, než dovoluje maximum";
+
+$lang_folder_cant_chmod = "Nemohu zmenit práva [perms] na složce [folder]";
+$lang_file_cant_chmod = "Nemohu zmenit práva [perms] na souboru [file]";
+$lang_chmod_max_777 = "Práva nemohou být vetší, než 777";
+$lang_chmod_owner = "Práva vlastníka";
+$lang_chmod_group = "Práva skupiny";
+$lang_chmod_public = "Práva verejná";
+$lang_chmod_manual = "Manuálne";
+$lang_chmod_read = "Císt";
+$lang_chmod_write = "Zapisovat";
+$lang_chmod_exe = "Spouštet";
+$lang_chmod_no_support = "Tento PHP server nepodporuje ftp_chmod";
+
+$lang_title_rename = "Prejmenovat soubor";
+$lang_title_chmod = "Zmenit atributy souboru";
+$lang_title_edit_file = "Upravuji";
+$lang_title_new_file = "Nový soubor";
+$lang_title_new_folder = "Nová složka";
+$lang_new_folder_name = "Zadej nové jméno složky...";
+$lang_new_file_name = "Zadej nové jméno souboru...";
+$lang_template = "Vzor";
+$lang_no_template = "Žádný vzor";
+
+$lang_no_xmlhttp = "Váš prohlížec nepodporuje XMLHTTP!";
+$lang_support_drop = "Podporováno";
+$lang_no_support_drop = "Nepodporováno";
+$lang_transfer_pending = "Cekám";
+$lang_transferring_to_ftp = "Odesílám na úložište...";
+$lang_no_file_selected = "Klikni na PROCHÁZET a vyber soubor k nahrání!";
+$lang_none_selected = "Prosím vyber alespon jeden soubor nebo složku!";
+$lang_context_open = "Otevrít";
+$lang_context_download = "Stáhnout";
+$lang_context_edit = "Upravit";
+$lang_context_cut = "Vyjmout";
+$lang_context_copy = "Kopírovat";
+$lang_context_paste = "Vložit";
+$lang_context_rename = "Prejmenovat";
+$lang_context_delete = "Smazat";
+$lang_context_chmod = "Práva";
+
+?>
