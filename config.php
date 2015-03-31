494da1043f986e7b3be2bf4427f655fd0c165c1b
Monsta FTP <monstaftp@users.noreply.github.com>
2015-04-01 12:32:54 +1300

    Create config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+$version = "1.5.2";
+
+# Open README file for descriptions and help.
+
+$ftpHost = "";
+$ftpPort = "21";
+$ftpMode = "1";
+$ftpSSL  = "0";
+$ftpDir  = "";
+$serverTmp = "/tmp";
+$editableExts = "asp,ashx,asmx,aspx,asx,axd,cfm,cgi,css,html,htm,jhtml,js,php,phtml,pl,txt,xhtml";
+$dateFormatUsa = 0;
+$lockOutTime = 5;
+$versionCheck = 1;
+
+?>
