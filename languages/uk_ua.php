d92c0d9b37d2dd73ecc0fc15da1d46a4d21803ed
Andrey <kolovskiy83@yahoo.com>
2015-05-03 20:19:40 +0300

    Create uk_ua.php

---
+++ languages/uk_ua.php
@@ -0,0 +1,146 @@
+<?php
+
+// Language: Ukrainian (RU)
+// Author: Crazistor
+// Last Updated: 03/05/2015
+
+$file_lang_name = "Українська (UK)";// 
+$file_lang_default = 0; // 1 for default, 0 for not
+
+$lang_title_ended = "Сесію завершено";
+$lang_title_errors = "Помилка";
+
+$lang_session_expired = "Термін дії сесії вичерпано";
+$lang_ip_conflict = "Конфлікт вашої IP з IP вашої сесії";
+
+$lang_btn_login = "Логін";
+$lang_ftp_host = "FTP сервер";
+$lang_port = "Порт";
+$lang_passive_mode = "Пасивний режим (за замовчуванням)";
+$lang_username = "Ім`я користувача";
+$lang_password = "Пароль";
+$lang_ftp_ssl = "Використовувати SSL (якщо доступно)";
+$lang_adv_interface = "Розширений інтерфейс";
+$lang_save_login = "Зберегти дані з`єднання";
+$lang_language = "Мова";
+$lang_skin = "Тема оформлення";
+$lang_ip_check = "Прив`язати сесію до IP";
+
+$lang_skins_empty = "[skins] тека пуста!";
+$lang_skins_locked = "[skins] тека заблокована!";
+$lang_skins_missing = "[skins] тека відсутня!";
+
+$lang_missing_fields = "Помилка! Будь-ласка, заповніть всі відмічені поля";
+$lang_max_logins = "Ви перевищили кількість дозволених спроб входу і були заблоковані. Будь-ласка, спробуйте знову через [n] хвилин";
+$lang_max_logins = "Перевищено ліміт спроб підключення. Будь-ласка, спробуйте знову через [n] хвилин";
+$lang_cant_connect = "Помилка! Неможливо підключитися до FTP серверу";
+$lang_cant_authenticate = "Помилка авторизації, перевірте логін і пароль";
+$lang_ip_conflict = "Конфлікт IP адрес";
+
+$lang_table_name = "Ім`я файла";
+$lang_table_size = "Розмір";
+$lang_table_date = "Дата";
+$lang_table_time = "Час";
+$lang_table_user = "Користувач";
+$lang_table_group = "Група";
+$lang_table_perms = "Дозволи";
+
+$lang_size_b = "Б";
+$lang_size_kb = "кБ";
+$lang_size_mb = "МБ";
+$lang_size_gb = "ГБ";
+$lang_size_tb = "ТБ";
+
+$lang_btn_refresh = "Оновити";
+$lang_btn_cut = "Вирізати";
+$lang_btn_copy = "Копіювати";
+$lang_btn_paste = "Вставити";
+$lang_btn_rename = "Перейменувати";
+$lang_btn_delete = "Видалити";
+$lang_btn_chmod = "Права доступу";
+$lang_btn_logout = "Вихід";
+$lang_btn_save = "Зберегти";
+$lang_btn_close = "Закрити";
+$lang_btn_cancel = "Скасувати";
+$lang_btn_ok = "OK";
+$lang_btn_new_folder = "Нова тека";
+$lang_btn_new_file = "Новий файл";
+$lang_btn_upload_file = "Завантажити файл на сервер";
+$lang_btn_upload_files = "Завантажити файли на сервер";
+$lang_btn_upload_repeat = "Повторити завантаження на сервер";
+$lang_btn_upload_folder = "Завантажити теку на сервер";
+
+$lang_info_host = "Сервер";
+$lang_info_user = "Користувач";
+$lang_info_upload_limit = "Лиміт завантажень на сервер";
+$lang_info_drag_drop = "Перетягнення файлів (Drag & Drop)"; 
+
+$lang_xfer_file = "Файл";
+$lang_xfer_size = "Розмір";
+$lang_xfer_progress = "Стан";
+$lang_xfer_elapsed = "Пройшло часу";
+$lang_xfer_uploaded = "Завантажено на сервер";
+$lang_xfer_rate = "Швидкість завантаження"; 
+$lang_xfer_remain = "Залишилось часу";
+
+$lang_move_conflict = "Тека призначення знаходиться всередині теки джерела";
+$lang_cant_rename = "Неможливо перейменувати [file]";
+$lang_cant_delete = "Неможливо видалити [file]";
+
+$lang_folder_exists = "Тека [folder] вже існує";
+$lang_folder_doesnt_exist = "Тека [folder] не існує";
+$lang_folder_cant_move = "Неможливо перемістити [folder]";
+$lang_folder_cant_delete = "Неможливо видалити [folder]";
+$lang_folder_cant_access = "Немає доступу до [folder]";
+$lang_folder_cant_make = "Неможливо створити [folder]";
+
+$lang_file_exists = "Файл [file] вже існує";
+$lang_file_doesnt_exist = "Файл [file] не існує";
+$lang_file_cant_move = "Неможливо перемістити [file]";
+$lang_file_cant_make = "Неможливо створити [file]";
+
+$lang_server_error_down = "Помилка сервера при скачуванні [file]";
+$lang_server_error_up = "Помилка сервера при завантаженні [file]";
+$lang_browser_error_up = "Помилка браузера при завантаженні [file]";
+$lang_file_size_error = "Розмір файла перевищує допустиме значення";
+$lang_file_size_copy_error = "[file] розмір перевищує допустиме значення";
+
+$lang_folder_cant_chmod = "Неможливо встановити права [perms] для [folder]";
+$lang_file_cant_chmod = "Неможливо встановити права [perms] для [file}";
+$lang_chmod_max_777 = "Chmod не може бути вище 777";
+$lang_chmod_owner = "Права доступу власника";
+$lang_chmod_group = "Права доступу групи";
+$lang_chmod_public = "Права доступу ынших";
+$lang_chmod_manual = "Встановити вручну";
+$lang_chmod_read = "Зчитування";
+$lang_chmod_write = "Запис";
+$lang_chmod_exe = "Виконання";
+
+$lang_title_rename = "Перейменувати файли";
+$lang_title_chmod = "Змынити атрибути файла";
+$lang_title_edit_file = "Правка";
+$lang_title_new_file = "Новий файл";
+$lang_title_new_folder = "Нова тека";
+$lang_new_folder_name = "Введіть ім`я нової теки...";
+$lang_new_file_name = "Введіть ім`я нового файла...";
+$lang_template = "Шаблон";
+$lang_no_template = "Без шаблона";
+
+$lang_no_xmlhttp = "Ваш браузер не підтримує XMLHTTP!";
+$lang_support_drop = "Ваш браузер підтримує Drag & Drop"; 
+$lang_no_support_drop = "Підтримка Drag & Drop відсутня"; 
+$lang_transfer_pending = "Очікування"; 
+$lang_transferring_to_ftp = "Завантаження на FTP сервер...";
+$lang_no_file_selected = "Натисніть кнопку Обзор та оберіть файл для завантаження!";
+$lang_none_selected = "Оберіть хоча б один файл або теку!";
+$lang_context_open = "Відкрити";
+$lang_context_download = "Завантаження з сервера";
+$lang_context_edit = "Правка";
+$lang_context_cut = "Вирізати";
+$lang_context_copy = "Копіювати";
+$lang_context_paste = "Вставити";
+$lang_context_rename = "Перйменувати";
+$lang_context_delete = "Видалити";
+$lang_context_chmod = "Права доступу";
+
+?>
