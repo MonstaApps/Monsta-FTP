02e37edecf61fbac7ed4494d7714e3275569d7c8
Daniel Falkner <daniel.falkner@telekom.de>
2015-07-07 15:10:07 +0200

    use home dir as start

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ $ftpPass = getenv('FTP_PASS');
 
 $ftpMode = "1";
 $ftpSSL  = "0";
-$ftpDir  = "";
+$ftpDir  = "~";
 $editableExts = "asp,ashx,asmx,aspx,asx,axd,cfm,cgi,css,html,htm,jhtml,js,php,phtml,pl,txt,xhtml";
 $serverTmp = "/tmp";
 $dateFormatUsa = 0;
