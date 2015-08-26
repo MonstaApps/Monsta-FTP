d3e0e132d96b195480fa0cfb869af24d232d55c8
Monsta FTP <monstaftp@users.noreply.github.com>
2015-08-26 13:58:02 +1200

    Create vi_vi.php

---
+++ languages/vi_vi.php
@@ -0,0 +1,149 @@
+<?php
+
+// Language: Viet Nam(VI)
+// Author: Đại Phúc Minh
+// Last Updated: 16/07/2013
+
+$file_lang_name = "Tiếng Việt (VI)";
+$file_lang_default = 0; // 1 for default, 0 for not
+
+$lang_title_ended = "Kết thúc quá trình";
+$lang_title_errors = "Lỗi xuất hiện";
+
+$lang_session_expired = "Quá trình làm việc đã bị tạm ngưng. Yêu Đăng nhập lại";
+$lang_ip_conflict = "Xung đột địa chỉ IP của bạn với phiên IP";
+
+$lang_btn_login = "ĐĂNG NHẬP";
+$lang_ftp_host = "IP Máy chủ hoặc tên website ";
+$lang_port = "Cổng";
+$lang_passive_mode = "Chế độ thụ động (mặc định)";
+$lang_username = "Tên đăng nhập FTP";
+$lang_password = "Mật khẩu người dùng";
+$lang_ftp_ssl = "Kết nối sử dụng SSL (nếu có)";
+$lang_adv_interface = "Giao tiếp nâng cao";
+$lang_save_login = "Nhớ mật khẩu cho các lần đăng sau";
+$lang_language = "Ngôn ngữ";
+$lang_skin = "Giao diện";
+$lang_ip_check = "IP Khóa phiên làm việc (Time uot)";
+
+$lang_skins_empty = "[skins] thư mục trống!";
+$lang_skins_locked = "[skins] thư mục bị khóa!";
+$lang_skins_missing = "[skins] thư mục bị mất!";
+
+$lang_missing_fields = "Lỗi đăng nhập! Hãy điền đầy đủ giá trị cần thiết vào các mục cần thiết";
+$lang_max_logins = "Bạn đã vượt quá những lần đăng nhập cho phép và đã được khóa. Vui lòng thử lại trong [n] phút";
+$lang_cant_connect = "Lỗi đăng nhập! Không thể kết nối đến máy chủ FTP. Nhập lại địa chỉ IP hoặc tên website cho chính xác";
+$lang_cant_authenticate = "LỖI ĐĂNG NHẬP! <br>
+Bạn đã nhập sai IP hoặc tài khoản hay mật khẩu <br>
+Có thể thư mục của bạn đã bị lỡ tay xóa nhầm.<br>
+Liên hệ với quản trị để tạo lại thư mục cho bạn !";
+$lang_ip_conflict = "Một cuộc xung đột IP đã được phát hiện";
+
+$lang_table_name = "Tên";
+$lang_table_size = "Kích thước";
+$lang_table_date = "Ngày tải lên";
+$lang_table_time = "Vào lúc";
+$lang_table_user = "ID Tài khoản";
+$lang_table_group = "Tên tài khoản";
+$lang_table_perms = "Quyền";
+
+$lang_size_b = "b";
+$lang_size_kb = "Kb";
+$lang_size_mb = "Mb";
+$lang_size_gb = "Gb";
+$lang_size_tb = "Tb";
+
+$lang_btn_refresh = "LÀM TƯƠI";
+$lang_btn_cut = "Cắt";
+$lang_btn_copy = "Sao chép";
+$lang_btn_paste = "Dán";
+$lang_btn_rename = "Đổi tên";
+$lang_btn_delete = "Xóa";
+$lang_btn_chmod = "Chmod";
+$lang_btn_logout = "ĐĂNG XUẤT";
+$lang_btn_save = "Lưu";
+$lang_btn_close = "Đóng";
+$lang_btn_cancel = "Thôi";
+$lang_btn_ok = "OK";
+$lang_btn_new_folder = "Tạo thư mục";
+$lang_btn_new_file = "Tạo File";
+$lang_btn_upload_file = "TẢI FILE LÊN";
+$lang_btn_upload_files = "TẢI FILE LÊN";
+$lang_btn_upload_repeat = "TẢI LẠI";
+$lang_btn_upload_folder = "TẢI THƯ MỤC LÊN";
+
+$lang_info_host = "Máy chủ";
+$lang_info_user = "Tài khoản";
+$lang_info_upload_limit = "Giới hạn tải lên mỗi file";
+$lang_info_drag_drop = "Kéo & Thả";
+
+$lang_xfer_file = "Tập tin";
+$lang_xfer_size = "Kích thước";
+$lang_xfer_progress = "Tiến độ";
+$lang_xfer_elapsed = "Thời gian trôi qua";
+$lang_xfer_uploaded = "Đã tải lên";
+$lang_xfer_rate = "Tốc độ truyền tải";
+$lang_xfer_remain = "Thời gian còn lại";
+
+$lang_move_conflict = "Thư mục đích là một thư mục con của thư mục nguồn";
+$lang_cant_rename = "Không thể đổi tên [file]";
+$lang_cant_delete = "Không thể xóa [file]";
+
+$lang_folder_exists = "Thư mục [folder] đã tồn tại";
+$lang_folder_doesnt_exist = "Thư mục [folder] không tồn tại";
+$lang_folder_cant_move = "Không thể di chuyển [folder]";
+$lang_folder_cant_delete = "Không thể xóa [folder]";
+$lang_folder_cant_access = "Không thể truy cập [folder]";
+$lang_folder_cant_make = "Không thể tạo [folder]";
+
+$lang_file_exists = "Tập tin [file] đã tồn tại";
+$lang_file_doesnt_exist = "Tập tin [file] không tồn tại";
+$lang_file_cant_move = "Không thể di chuyển [file]";
+$lang_file_cant_make = "Không thể tạo[file]";
+
+$lang_server_error_down = "Lỗi máy chủ tải xuống [file]";
+$lang_server_error_up = "Lỗi máy chủ tải lên [file]";
+$lang_browser_error_up = "Duyệt lỗi khi tải lên [file]";
+$lang_file_size_error = "Kích thước tập tin vượt quá giới hạn cho phép";
+$lang_file_size_copy_error = "[file]kích thước vượt quá giới hạn cho phép";
+
+$lang_folder_cant_chmod = "Không thể chmod [perms] [folder]";
+$lang_file_cant_chmod = "Không thể chmod[perms] [file]";
+$lang_chmod_max_777 = "Chmod thể không được cao hơn 777";
+$lang_chmod_owner = "Quyền hạn chủ sở hữu ";
+$lang_chmod_group = "Quyền hạn Nhóm";
+$lang_chmod_public = "Quyền hạn công khai";
+$lang_chmod_manual = "Hướng dẫn";
+$lang_chmod_read = "Đọc";
+$lang_chmod_write = "Viết";
+$lang_chmod_exe = "Thi hành";
+$lang_chmod_no_support = "Máy chủ PHP của bạn không hỗ trợ ftp_chmod";
+
+$lang_title_rename = "Đổi tên Files";
+$lang_title_chmod = "Sửa thuộc tính tập tin";
+$lang_title_edit_file = "Chỉnh sửa";
+$lang_title_new_file = "Tập tin mới";
+$lang_title_new_folder = "Thư mục mới";
+$lang_new_folder_name = "Nhập tên thư mục mới ...";
+$lang_new_file_name = "Nhập tên tập tin mới ...";
+$lang_template = "Bản mẫu";
+$lang_no_template = "Không mẫu";
+
+$lang_no_xmlhttp = "Trình duyệt của bạn không hỗ trợ XMLHTTP!";
+$lang_support_drop = "Được hỗ trợ";
+$lang_no_support_drop = "Không được hỗ trợ";
+$lang_transfer_pending = "Đang chờ";
+$lang_transferring_to_ftp = "Chuyển đến máy chủ FTP ...";
+$lang_no_file_selected = "Vui lòng nhấn vào Browse và chọn một tập tin để tải lên!";
+$lang_none_selected = "Vui lòng chọn ít nhất một tập tin hoặc thư mục!";
+$lang_context_open = "Mở";
+$lang_context_download = "Tải xuống";
+$lang_context_edit = "Sửa";
+$lang_context_cut = "Cắt";
+$lang_context_copy = "Sao chép";
+$lang_context_paste = "Dán";
+$lang_context_rename = "Đổi tên";
+$lang_context_delete = "Xóa";
+$lang_context_chmod = "Chmod";
+
+?>
