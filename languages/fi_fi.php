4adf92799f1a04b9a84bca060534be886ee8679f
Monsta FTP <monstaftp@users.noreply.github.com>
2016-01-13 13:46:46 +1300

    Create fi_fi.php

---
+++ languages/fi_fi.php
@@ -0,0 +1,147 @@
+<?php
+
+// Language: Finnish (FI)
+// Author: Tomas Nybondas
+// Last Updated: 04/01/2016
+
+$file_lang_name = "Finnish (FI)";
+$file_lang_default = 0; // 1 for default, 0 for not
+
+$lang_title_ended = "Istunto p&auml;&auml;ttyi";
+$lang_title_errors = "Virheit&auml; havaittu";
+
+$lang_session_expired = "Istunto on p&auml;&auml;ttynyt";
+$lang_ip_conflict = "IP-osoitteesi ei ole sama kuin istunnon IP-osoite";
+
+$lang_btn_login = "Kirjaudu";
+$lang_ftp_host = "FTP-palvelin";
+$lang_port = "Portti";
+$lang_passive_mode = "Passiivinen tila (oletus)";
+$lang_username = "K&auml;ytt&auml;j&auml;tunnus";
+$lang_password = "Salasana";
+$lang_ftp_ssl = "Kytkeydy k&auml;ytt&auml;en SSL:ia (mik&auml;li k&auml;ytett&auml;viss&auml;)";
+$lang_adv_interface = "Edistyneet toiminnot";
+$lang_save_login = "Tallenna kirjautumistiedot";
+$lang_language = "Kieli";
+$lang_skin = "Ulkoasu";
+$lang_ip_check = "Lukitse istunto IP-osoitteeseen";
+
+$lang_skins_empty = "[skins] -kansio on tyhj&auml;!";
+$lang_skins_locked = "[skins] -kansio lukittu!";
+$lang_skins_missing = "[skins] -kansio puuttuu!";
+
+$lang_missing_fields = "Virhe! T&auml;yt&auml; kaikki kent&auml;t.";
+$lang_max_logins = "Maksimi m&auml;&auml;r&auml; virheellisi&auml; kirjautumisyrityksi&auml; t&auml;yttyi. Yrit&auml; uudelleen [n] minuutin kuluttua";
+$lang_cant_connect = "Virhe! FTP-palvelimeen ei saada yhteytt&auml;";
+$lang_cant_authenticate = "Virhe! Kirjautuminen ei onnistunut";
+$lang_ip_conflict = "IP-osoiteriita havaittu";
+
+$lang_table_name = "Nimi";
+$lang_table_size = "Koko";
+$lang_table_date = "P&auml;iv&auml;ys";
+$lang_table_time = "Aika";
+$lang_table_user = "K&auml;ytt&auml;j&auml;";
+$lang_table_group = "Ryhm&auml;";
+$lang_table_perms = "Oikeudet";
+
+$lang_size_b = "B";
+$lang_size_kb = "KB";
+$lang_size_mb = "MB";
+$lang_size_gb = "GB";
+$lang_size_tb = "TB";
+
+$lang_btn_refresh = "P&auml;ivit&auml;";
+$lang_btn_dl = "Lataa";
+$lang_btn_cut = "Leikkaa";
+$lang_btn_copy = "Kopioi";
+$lang_btn_paste = "Liit&auml;";
+$lang_btn_rename = "Nime&auml; uudelleen";
+$lang_btn_delete = "Poista";
+$lang_btn_chmod = "Chmod";
+$lang_btn_logout = "Kirjaudu ulos";
+$lang_btn_save = "Tallenna";
+$lang_btn_close = "Sulje";
+$lang_btn_cancel = "Peruuta";
+$lang_btn_ok = "OK";
+$lang_btn_new_folder = "Uusi kansio";
+$lang_btn_new_file = "Uusi tiedosto";
+$lang_btn_upload_file = "Siirr&auml; tiedosto";
+$lang_btn_upload_files = "Siirr&auml; tiedostoja";
+$lang_btn_upload_repeat = "Toista siirto";
+$lang_btn_upload_folder = "Siirr&auml; kansio";
+
+$lang_info_host = "Palvelin";
+$lang_info_user = "K&auml;ytt&auml;j&auml;";
+$lang_info_upload_limit = "Siirtorajoitus";
+$lang_info_drag_drop = "Ved&auml; & pudota";
+
+$lang_xfer_file = "Tiedosto";
+$lang_xfer_size = "Koko";
+$lang_xfer_progress = "Edistyminen";
+$lang_xfer_elapsed = "Aikaa kulunut";
+$lang_xfer_uploaded = "Siirretty";
+$lang_xfer_rate = "Siirtonopeus";
+$lang_xfer_remain = "Aikaa j&auml;ljell&auml;";
+
+$lang_move_conflict = "Kohdekansio on l&auml;hdekansion alla";
+$lang_cant_rename = "Ei voi uudelleennimet&auml; [file]";
+$lang_cant_delete = "Ei voi poistaa [file]";
+
+$lang_folder_exists = "Kansio [folder] on jo olemassa";
+$lang_folder_doesnt_exist = "Kansiota [folder] ei l&ouml;ydy";
+$lang_folder_cant_move = "Ei voi siirt&auml;&auml; kansiota [folder]";
+$lang_folder_cant_delete = "Ei voi poistaa kansiota [folder]";
+$lang_folder_cant_access = "P&auml;&auml;sy kansioon [folder] estetty";
+$lang_folder_cant_make = "Ei voi luoda kansiota [folder]";
+
+$lang_file_exists = "Tiedosto [file] on jo olemassa";
+$lang_file_doesnt_exist = "Tiedostoa [file] ei l&ouml;ydy";
+$lang_file_cant_move = "Ei voi siirt&auml;&auml; tiedostoa [file]";
+$lang_file_cant_make = "Tiedoston [file] luonti ei onnistu";
+
+$lang_server_error_down = "Palvelinvirhe tiedoston [file] lataamisessa";
+$lang_server_error_up = "Palvelinvirhe tiedoston [file] siirt&auml;misess&auml;";
+$lang_browser_error_up = "Selainvirhe tiedoston [file] siirt&auml;misess&auml;";
+$lang_file_size_error = "Tiedoston koko ylitt&auml;&auml; sallitun rajan";
+$lang_file_size_copy_error = "[file] ylitt&auml;&auml; sallitun rajan";
+
+$lang_folder_cant_chmod = "Chmod [perms] kansiolle [folder] ei onnistu";
+$lang_file_cant_chmod = "Chmod [perms] tiedostolle [file] ei onnistu";
+$lang_chmod_max_777 = "Chmod ei voi olla suurempi kuin 777";
+$lang_chmod_owner = "Omistajan oikeudet";
+$lang_chmod_group = "Ryhm&auml;n oikeudet";
+$lang_chmod_public = "Yleiset oikeudet";
+$lang_chmod_manual = "Ohje";
+$lang_chmod_read = "Luku";
+$lang_chmod_write = "Kirjoitus";
+$lang_chmod_exe = "Toteuta";
+$lang_chmod_no_support = "K&auml;ytetty PHP-palvelin ei tue ftp_chmod:ia";
+
+$lang_title_rename = "Uudelleennime&auml; tiedostoja";
+$lang_title_chmod = "Vaihda tiedostoattribuutteja";
+$lang_title_edit_file = "Muokkaus";
+$lang_title_new_file = "Uusi tiedosto";
+$lang_title_new_folder = "Uusi kansio";
+$lang_new_folder_name = "Anna uusi kansion nimi...";
+$lang_new_file_name = "Anna uusi tiedoston nimi...";
+$lang_template = "Malli";
+$lang_no_template = "Ei mallia";
+
+$lang_no_xmlhttp = "Selaimesi ei tue XMLHTTP:ta!";
+$lang_support_drop = "Tuettu";
+$lang_no_support_drop = "Ei tuettu";
+$lang_transfer_pending = "Odottaa";
+$lang_transferring_to_ftp = "Siirret&auml;&auml;n FTP-palvelimella...";
+$lang_no_file_selected = "Klikkaa BROWSE valitaksesi siirrett&auml;v&auml;n tiedoston!";
+$lang_none_selected = "Valitse v&auml;hint&auml;&auml;n yksi tiedosto tai kansio!";
+$lang_context_open = "Avaa";
+$lang_context_download = "Lataa";
+$lang_context_edit = "Muokkaa";
+$lang_context_cut = "Leikkaa";
+$lang_context_copy = "Kopioi";
+$lang_context_paste = "Liit&auml;";
+$lang_context_rename = "Nime&auml; uudelleen";
+$lang_context_delete = "Poista";
+$lang_context_chmod = "Chmod";
+
+?>
