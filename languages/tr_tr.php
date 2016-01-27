546ba32b8b039811ff624517eac83c7d7bf2609f
Monsta FTP <monstaftp@users.noreply.github.com>
2016-01-27 16:40:45 +1300

    Create tr_tr.php

---
+++ languages/tr_tr.php
@@ -0,0 +1,147 @@
+<?php
+
+// Language: Türkçe (TR)
+// Author: Ahmet Gök
+// Last Updated: 21 Jan, 2016
+
+$file_lang_name = "Türkçe (Tr)";
+$file_lang_default = 0; // 1 for default, 0 for not
+
+$lang_title_ended = "Oturum Kapatıldı";
+$lang_title_errors = "hatalar oldu";
+
+$lang_session_expired = "Oturum Süresi Doldu";
+$lang_ip_conflict = "Oturum açtiğiniz İp Adresiniz ile Çakişma var";
+
+$lang_btn_login = "Giriş";
+$lang_ftp_host = "FTP Host Adresi";
+$lang_port = "Port";
+$lang_passive_mode = "Pasif Mod (default)";
+$lang_username = "Kullanici Adi";
+$lang_password = "Şifre";
+$lang_ftp_ssl = "SSL kullanarak baglan (Eğer Varsa)";
+$lang_adv_interface = "Gelişmiş Arayüzü";
+$lang_save_login = "Giriş bilgilerini kaydet";
+$lang_language = "Dil";
+$lang_skin = "Görünüm";
+$lang_ip_check = "IP oturumu kilitlemek";
+
+$lang_skins_empty = "[skins] Klasör boş!";
+$lang_skins_locked = "[skins] Klasör kilitli!";
+$lang_skins_missing = "[skins] Klasör eksik!";
+
+$lang_missing_fields = "Hata! Gerekli bütün alanları doldurun";
+$lang_max_logins = "Hatalı oturum açma girişimlerini aştınız ve kilitlendi. [n] dakika içinde tekrar deneyin";
+$lang_cant_connect = "Hata! FTP hosta Bağlanılamadı ";
+$lang_cant_authenticate = "Hata! Giriş için kimlik doğrulaması yapılamadı Bilgilerinizi Gözden geçiriniz";
+$lang_ip_conflict = "Bir IP çakışması tespit edildi";
+
+$lang_table_name = "İsim";
+$lang_table_size = "Boyut";
+$lang_table_date = "Tarih";
+$lang_table_time = "Zaman";
+$lang_table_user = "Kullanıcı";
+$lang_table_group = "Grup";
+$lang_table_perms = "İzinler";
+
+$lang_size_b = "B";
+$lang_size_kb = "KB";
+$lang_size_mb = "MB";
+$lang_size_gb = "GB";
+$lang_size_tb = "TB";
+
+$lang_btn_refresh = "Yenile";
+$lang_btn_dl = "İndir";
+$lang_btn_cut = "Kes";
+$lang_btn_copy = "Kopyala";
+$lang_btn_paste = "Yapiştir";
+$lang_btn_rename = "Değiştir";
+$lang_btn_delete = "Sil";
+$lang_btn_chmod = "İzin";
+$lang_btn_logout = "Çikiş";
+$lang_btn_save = "Kayıt";
+$lang_btn_close = "Çik";
+$lang_btn_cancel = "Geri";
+$lang_btn_ok = "Tamam";
+$lang_btn_new_folder = "Yeni Klasör";
+$lang_btn_new_file = "Yeni Dosya";
+$lang_btn_upload_file = "Dosya Yükle";
+$lang_btn_upload_files = "Dosyalar Yükle";
+$lang_btn_upload_repeat = "Tekrar Yükle";
+$lang_btn_upload_folder = "Klasör Yükle";
+
+$lang_info_host = "Sunucu";
+$lang_info_user = "Kullanıcı";
+$lang_info_upload_limit = "Yükleme Limiti";
+$lang_info_drag_drop = "Sürükle& Bırak";
+
+$lang_xfer_file = "Dosya";
+$lang_xfer_size = "Boyut";
+$lang_xfer_progress = "İlerleme";
+$lang_xfer_elapsed = "Geçen Süre";
+$lang_xfer_uploaded = "Yüklenen";
+$lang_xfer_rate = "Yükleme Hızı";
+$lang_xfer_remain = "Geriye Kalan Süre";
+
+$lang_move_conflict = "hedef klasörün alt atl klasörleri";
+$lang_cant_rename = "Adlandırılamıyor [Dosya]";
+$lang_cant_delete = "Silinemiyor[Dosya]";
+
+$lang_folder_exists = "Folder [folder] already exists";
+$lang_folder_doesnt_exist = "Folder [folder] doesn't exist";
+$lang_folder_cant_move = "Unable to move [folder]";
+$lang_folder_cant_delete = "Unable to delete [folder]";
+$lang_folder_cant_access = "Unable to access [folder]";
+$lang_folder_cant_make = "Unable to create [folder]";
+
+$lang_file_exists = "Dosya [Dosya] Zaten var";
+$lang_file_doesnt_exist = "Dosya [Dosya] mevcut değildir";
+$lang_file_cant_move = "Taşınamıyor [Dosya]";
+$lang_file_cant_make = "Oluşturulamıyor[Dosya]";
+
+$lang_server_error_down = "Sunucudan indirme hatası [Dosya]";
+$lang_server_error_up = "Sunucuya yükleme hatası[Dosya]";
+$lang_browser_error_up = "Tarayıcı yükleme hatası [Dosya]";
+$lang_file_size_error = "Dosya boyutu izin verilen sınırı aşıyor";
+$lang_file_size_copy_error = "[Dosya] boyut izin sınırını aşıyor";
+
+$lang_folder_cant_chmod = "izin verilmiyor [perms] [Klasör]";
+$lang_file_cant_chmod = "İzin verilmiyor[perms] [Dosya]";
+$lang_chmod_max_777 = "izin 777 dan büyük olamaz";
+$lang_chmod_owner = "Yükleyen için izinler";
+$lang_chmod_group = "Grup izinleri";
+$lang_chmod_public = "herkes izinler";
+$lang_chmod_manual = "El ile";
+$lang_chmod_read = "Okuma";
+$lang_chmod_write = "Yazma";
+$lang_chmod_exe = "Değiştirmek";
+$lang_chmod_no_support = "Sizin PHP serveriniz ftp_izin desteklemiyor";
+
+$lang_title_rename = "Dosya yeniden adlandırma";
+$lang_title_chmod = "Dosya değiştirme izinleri";
+$lang_title_edit_file = "Düzenleme";
+$lang_title_new_file = "Yeni Dosya";
+$lang_title_new_folder = "Yeni Klasör";
+$lang_new_folder_name = "Yeni Klasör ismi...";
+$lang_new_file_name = "Yeni Dsoya ismi...";
+$lang_template = "Şema";
+$lang_no_template = "Şema yok";
+
+$lang_no_xmlhttp = "Tarayıcınız XMLHTTP desteklemiyor!";
+$lang_support_drop = "Destekliyor";
+$lang_no_support_drop = "Desteklenmiyor";
+$lang_transfer_pending = "Bekleyiniz";
+$lang_transferring_to_ftp = " FTP Servere Yükleniyor...";
+$lang_no_file_selected = "Lütfen ! Gözat'ı tıklatın ve yüklemek için bir dosya seçiniz!";
+$lang_none_selected = "Lütfen En az bir dosya veya klasör seçiniz!";
+$lang_context_open = "Aç";
+$lang_context_download = "İndir";
+$lang_context_edit = "Düzenle";
+$lang_context_cut = "Kes";
+$lang_context_copy = "Kopyala";
+$lang_context_paste = "Yapiştir";
+$lang_context_rename = "Adlandir";
+$lang_context_delete = "Sil";
+$lang_context_chmod = "İzinler";
+
+?>
