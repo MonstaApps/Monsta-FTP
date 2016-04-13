f8f036c92bb9aa35c4851acce473307e75f3769a
Monsta <MonstaApps@users.noreply.github.com>
2016-04-14 10:27:04 +1200

    Update uk_ua.php

--- languages/uk_ua.php
+++ languages/uk_ua.php
@@ -144,4 +144,12 @@ $lang_context_rename = "Перйменувати";
 $lang_context_delete = "Видалити";
 $lang_context_chmod = "Права доступу";
 
+// Translation required (start)
+$lang_title_fetch_file = "Fetch File";
+$lang_fetch_no_file = "The URL does not contain a file name.";
+$lang_fetch_not_found = "No file could be found at this address.";
+$lang_btn_fetch = "Fetch";
+$lang_btn_fetch_file = "Fetch File";
+// Translation required (end)
+
 ?>
