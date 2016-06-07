898eab60fa7f15e408424eee4e9b0b1f16892f5f
Monsta <MonstaApps@users.noreply.github.com>
2016-06-07 13:58:43 +1200

    Create ro_ro.php

---
+++ languages/ro_ro.php
@@ -0,0 +1,152 @@
+<?php
+
+// Language: Romanian (RO)
+// Author: Grifasi Montana
+// Last Updated: 29 Apr, 2016
+
+$file_lang_name = "Romana (RO)";
+$file_lang_default = 1; // 1 pentru implicit, 0 pentru nu
+
+$lang_title_ended = "Sesiune terminata";
+$lang_title_errors = "A aparut o eroare!";
+
+$lang_session_expired = "Sesiunea a expirat";
+$lang_ip_conflict = "IP-ul tau intra in conflict cu IP-ul sesiunii";
+
+$lang_btn_login = "Autentificare";
+$lang_ftp_host = "Gazda FTP";
+$lang_port = "Port";
+$lang_passive_mode = "Mod pasiv (implicit)";
+$lang_username = "Utilizator";
+$lang_password = "Parola";
+$lang_ftp_ssl = "Conecteaza-te folosind SSL (daca este disponibil)";
+$lang_adv_interface = "Interfata avansata";
+$lang_save_login = "Salveaza datele de logare";
+$lang_language = "Limba";
+$lang_skin = "Interfata";
+$lang_ip_check = "Blocheaza sesiunea pe IP";
+
+$lang_skins_empty = "[skins] dosar gol!";
+$lang_skins_locked = "[skins] dosar blocat!";
+$lang_skins_missing = "[skins] dosar lipsa!";
+
+$lang_missing_fields = "Eroare! Te rugam sa complectati campurile obligatorii";
+$lang_max_logins = "Ai depasit limita de tentative de logare admisa si ai fost blocat temporar. Te rugam ds incerci peste [n] minute";
+$lang_cant_connect = "Eroare! Nu reusesc sa ma conectez la FTP";
+$lang_cant_authenticate = "Eroare! Numele de utilizator sau parola sunt gresite";
+$lang_ip_conflict = "Un conflict de IP-uri a fost detectat";
+
+$lang_table_name = "Nume";
+$lang_table_size = "Marime";
+$lang_table_date = "Data";
+$lang_table_time = "Timp";
+$lang_table_user = "Utilizator";
+$lang_table_group = "Grup";
+$lang_table_perms = "Permisiuni";
+
+$lang_size_b = "B";
+$lang_size_kb = "KB";
+$lang_size_mb = "MB";
+$lang_size_gb = "GB";
+$lang_size_tb = "TB";
+
+$lang_btn_refresh = "Reimprospatare";
+$lang_btn_dl = "Descarca";
+$lang_btn_cut = "Taie";
+$lang_btn_copy = "Copiaza";
+$lang_btn_paste = "Lipeste";
+$lang_btn_rename = "Redenumeste";
+$lang_btn_delete = "Sterge";
+$lang_btn_chmod = "Chmod";
+$lang_btn_logout = "Deconectare";
+$lang_btn_save = "Salveaza";
+$lang_btn_close = "Inchide";
+$lang_btn_cancel = "Renunta";
+$lang_btn_ok = "OK";
+$lang_btn_new_folder = "Dosar nou";
+$lang_btn_new_file = "Fisier nou";
+$lang_btn_upload_file = "Incarca fisier";
+$lang_btn_upload_files = "Incarca fisiere";
+$lang_btn_upload_repeat = "Repeta incarcare";
+$lang_btn_upload_folder = "Incarca dosar";
+
+$lang_info_host = "Gazda";
+$lang_info_user = "Utilizator";
+$lang_info_upload_limit = "Limita incarcare";
+$lang_info_drag_drop = "Drag & Drop";
+
+$lang_xfer_file = "Fisier";
+$lang_xfer_size = "Marime";
+$lang_xfer_progress = "Progres";
+$lang_xfer_elapsed = "Timp lipsa";
+$lang_xfer_uploaded = "Incarcat";
+$lang_xfer_rate = "Rata transfer";
+$lang_xfer_remain = "Timp ramas";
+
+$lang_move_conflict = "Dosarul de destinatie este un subdosar a unui dosar sursa";
+$lang_cant_rename = "Permisiuni insuficiente pentru a redenumii fisierul [file]";
+$lang_cant_delete = "Permisiuni insuficiente pentru a sterge fisierul [file]";
+
+$lang_folder_exists = "Dosarul [folder] exista deja";
+$lang_folder_doesnt_exist = "Dosarul [folder] nu exista";
+$lang_folder_cant_move = "Permisiuni insuficiente pentru a muta [folder]";
+$lang_folder_cant_delete = "Permisiuni insuficiente pentru a sterge [folder]";
+$lang_folder_cant_access = "Permisiuni insuficiente pentru a accesa [folder]";
+$lang_folder_cant_make = "Permisiuni insuficiente pentru a crea [folder]";
+
+$lang_file_exists = "Fisierul [file] exista deja";
+$lang_file_doesnt_exist = "Fisierul [file] nu exista";
+$lang_file_cant_move = "Permisiuni insuficiente pentru a muta [file]";
+$lang_file_cant_make = "Permisiuni insuficiente pentru a crea [file]";
+
+$lang_server_error_down = "Eroare la descarcare [file]";
+$lang_server_error_up = "Eroare la incarcare [file]";
+$lang_browser_error_up = "Eroare la browser la incarcare [file]";
+$lang_file_size_error = "Fisierul depaseste dimensiunea maxima permisa";
+$lang_file_size_copy_error = "[file] dimensiunea fisierului depaseste limita permisa";
+
+$lang_folder_cant_chmod = "Imposibil sa chmod [perms] [folder]";
+$lang_file_cant_chmod = "Imposibil sa chmod [perms] [file]";
+$lang_chmod_max_777 = "Chmod nu poate fi mai mare de 777";
+$lang_chmod_owner = "Permisiune Proprietar";
+$lang_chmod_group = "Permisiune Group";
+$lang_chmod_public = "Permisiune Publica";
+$lang_chmod_manual = "Manual";
+$lang_chmod_read = "Citeste";
+$lang_chmod_write = "Scrie";
+$lang_chmod_exe = "Executa";
+
+$lang_title_rename = "Redenumeste fisierele";
+$lang_title_chmod = "Schimba atributele fisierelor";
+$lang_title_edit_file = "Editeaza";
+$lang_title_new_file = "Fisier nou";
+$lang_title_new_folder = "Folder nou";
+$lang_new_folder_name = "Introdu numele noului folder...";
+$lang_new_file_name = "Introdu numele noului fisier...";
+$lang_template = "Tema";
+$lang_no_template = "Fara tema";
+
+$lang_no_xmlhttp = "Browserul tau nu suporta XMLHTTP!";
+$lang_support_drop = "Suportat";
+$lang_no_support_drop = "Nesuportat";
+$lang_transfer_pending = "In asteptare";
+$lang_transferring_to_ftp = "Transfer in curs catre serverul FTP...";
+$lang_no_file_selected = "Apasa pe Cauta si alege fisierul de incarcat!";
+$lang_none_selected = "Selecteaza cel putin un fisier din dosar!";
+$lang_context_open = "Deschide";
+$lang_context_download = "Descarca";
+$lang_context_edit = "Editeaza";
+$lang_context_cut = "Taie";
+$lang_context_copy = "Copiaza";
+$lang_context_paste = "Lipeste";
+$lang_context_rename = "Redenumeste";
+$lang_context_delete = "Sterge";
+$lang_context_chmod = "Chmod";
+
+$lang_title_fetch_file = "Preia fisier";
+$lang_fetch_no_file = "URL-ul nu contine un nume de fisier.";
+$lang_fetch_not_found = "Nici un fisier nu a putut fi gasit la aceasta adresa.";
+$lang_btn_fetch = "Preia";
+$lang_btn_fetch_file = "Preia fisier";
+
+?>
