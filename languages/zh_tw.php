c40ee5648b46f103cef1ff95e10ccc3cd8028668
inker610566 <inker610566@yahoo.com.tw>
2016-08-10 14:36:56 +0800

    Create zh_tw.php

---
+++ languages/zh_tw.php
@@ -0,0 +1,155 @@
+<?php
+
+// Language: Traditional Chinese (TW)
+// Author: inker610566@gmail.com
+// Last Updated: 10 Augest, 2016
+
+$file_lang_name = "繁體中文 (TW)";
+$file_lang_default = 0; // 1 for default, 0 for not
+
+$lang_title_ended = "會話結束";
+$lang_title_errors = "發生錯誤";
+
+$lang_session_expired = "您的登入會話已到期";
+$lang_ip_conflict = "您的IP地址和登入會話IP地址發生衝突";
+$lang_token_conflict = "偵測到跨站請求偽造攻擊";
+
+$lang_btn_login = "登入";
+$lang_ftp_host = "FTP 伺服器";
+$lang_port = "連接埠號碼";
+$lang_passive_mode = "被動模式(預設)";
+$lang_username = "使用者名稱";
+$lang_password = "密碼";
+$lang_ftp_ssl = "透過SSL連接(如果有的話)";
+$lang_adv_interface = "進階介面";
+$lang_save_login = "記錄登入資訊";
+$lang_language = "語言";
+$lang_skin = "主題樣式";
+$lang_ip_check = "綁定登入會話至IP";
+
+$lang_skins_empty = "[skins] 資料夾為空!";
+$lang_skins_locked = "[skins] 資料夾被鎖住!";
+$lang_skins_missing = "[skins] 資料夾遺失！";
+
+$lang_missing_fields = "錯誤！請完整填寫所有必要資料欄位";
+$lang_max_logins = "超出最大嘗試登入錯誤次數，帳號被鎖定，請過[n]分鐘後重試";
+$lang_cant_connect = "錯誤！無法連接FTP伺服器";
+$lang_cant_authenticate = "錯誤！無法驗證您的登入資料";
+$lang_ip_conflict = "發現IP地址衝突";
+
+$lang_table_name = "名稱";
+$lang_table_size = "大小";
+$lang_table_date = "日期";
+$lang_table_time = "時間";
+$lang_table_user = "使用者";
+$lang_table_group = "使用者群組";
+$lang_table_perms = "權限";
+
+$lang_size_b = "B";
+$lang_size_kb = "KB";
+$lang_size_mb = "MB";
+$lang_size_gb = "GB";
+$lang_size_tb = "TB";
+
+$lang_btn_refresh = "更新頁面";
+$lang_btn_dl = "下載";
+$lang_btn_cut = "剪下";
+$lang_btn_copy = "複製";
+$lang_btn_paste = "貼上";
+$lang_btn_rename = "重新命名";
+$lang_btn_delete = "刪除";
+$lang_btn_chmod = "更改權限";
+$lang_btn_logout = "登出";
+$lang_btn_save = "存檔";
+$lang_btn_close = "關閉";
+$lang_btn_cancel = "取消";
+$lang_btn_ok = "確定";
+$lang_btn_new_folder = "新增資料夾";
+$lang_btn_new_file = "新增檔案";
+$lang_btn_upload_file = "上傳單一檔案";
+$lang_btn_upload_files = "上傳多個檔案";
+$lang_btn_upload_repeat = "重新上傳";
+$lang_btn_upload_folder = "上傳資料夾";
+
+$lang_info_version = "版本";
+$lang_info_host = "伺服器";
+$lang_info_user = "使用者";
+$lang_info_upload_limit = "上傳限制";
+$lang_info_drag_drop = "直接拖曳";
+
+$lang_xfer_file = "檔案";
+$lang_xfer_size = "大小";
+$lang_xfer_progress = "進度";
+$lang_xfer_elapsed = "已花費時間";
+$lang_xfer_uploaded = "上傳完成";
+$lang_xfer_rate = "傳輸速度";
+$lang_xfer_remain = "剩餘時間";
+
+$lang_move_conflict = "目標資料夾是來源資料夾的子資料夾";
+$lang_cant_rename = "無法重新命名 [file]";
+$lang_cant_delete = "無法刪除 [file]";
+
+$lang_folder_exists = "資料夾 [folder] 已存在";
+$lang_folder_doesnt_exist = "資料夾 [folder] 不存在";
+$lang_folder_cant_move = "無法移動 [folder]";
+$lang_folder_cant_delete = "無法刪除 [folder]";
+$lang_folder_cant_access = "無法存取 [folder]";
+$lang_folder_cant_make = "無法新增 [folder]";
+
+$lang_file_exists = "檔案 [file] 已存在";
+$lang_file_doesnt_exist = "檔案 [file] 不存在";
+$lang_file_cant_move = "無法移動 [file]";
+$lang_file_cant_make = "無法新增 [file]";
+
+$lang_server_error_down = "伺服器下載 [file] 錯誤";
+$lang_server_error_up = "伺服器上傳 [file] 錯誤";
+$lang_browser_error_up = "瀏覽器上傳錯誤 [file]";
+$lang_file_size_error = "檔案大小超出限制";
+$lang_file_size_copy_error = "[file] 大小超出限制";
+
+$lang_folder_cant_chmod = "無法變更資料夾權限 [perms] [folder]";
+$lang_file_cant_chmod = "無法變更檔案權限 [perms] [file}";
+$lang_chmod_max_777 = "權限設定不能超過777";
+$lang_chmod_owner = "擁有者權限";
+$lang_chmod_group = "群組權限";
+$lang_chmod_public = "公共權限";
+$lang_chmod_manual = "文檔";
+$lang_chmod_read = "讀取";
+$lang_chmod_write = "寫入";
+$lang_chmod_exe = "執行";
+
+$lang_title_rename = "檔案重新命名";
+$lang_title_chmod = "改變檔案權限";
+$lang_title_edit_file = "編輯";
+$lang_title_new_file = "新檔案";
+$lang_title_new_folder = "新資料夾";
+
+$lang_new_folder_name = "輸入新檔案名稱...";
+$lang_new_file_name = "輸入新資料夾名稱...";
+$lang_template = "樣板";
+$lang_no_template = "沒有樣板";
+
+$lang_no_xmlhttp = "您的瀏覽器不支援 XMLHTTP!";
+$lang_support_drop = "支援";
+$lang_no_support_drop = "不支援";
+$lang_transfer_pending = "待完成";
+$lang_transferring_to_ftp = "傳輸至FTP伺服器...";
+$lang_no_file_selected = "請點選瀏覽鍵並選擇檔案上傳";
+$lang_none_selected = "請至少選擇一個檔案或資料夾!";
+$lang_context_open = "開啟";
+$lang_context_download = "下載";
+$lang_context_edit = "修改";
+$lang_context_cut = "剪下";
+$lang_context_copy = "複製";
+$lang_context_paste = "貼上";
+$lang_context_rename = "重新命名";
+$lang_context_delete = "刪除";
+$lang_context_chmod = "更改權限";
+
+$lang_title_fetch_file = "抓取檔案";
+$lang_fetch_no_file = "網址不包含任何檔案名稱.";
+$lang_fetch_not_found = "網址下找不到任何檔案.";
+$lang_btn_fetch = "抓取";
+$lang_btn_fetch_file = "抓取檔案";
+
+?>
